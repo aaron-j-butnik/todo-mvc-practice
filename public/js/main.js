fd39e249df5ad3d691c1fb002fceebcfad133b8e
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-23 14:12:34 -0700

    Add put /markIncomplete router in routes/todos.js.  Added markIncomplete method for controller in controllers/todos.js.  Added a fetch  async function markIncomplete() in public/js/main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,36 @@
 const todoItem = document.querySelectorAll("span.not");
+const todoComplete = document.querySelectorAll("span.completed");
 
-Array.from(todoItem).forEach((e) => {
+todoItem.forEach((e) => {
   e.addEventListener("click", markComplete);
+});
+
+todoComplete.forEach((e) => {
+  e.addEventListener("click", markIncomplete);
 });
 
 async function markComplete() {
   const todoId = this.parentNode.dataset.id;
   try {
     const response = await fetch("todos/markComplete", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todoIdFromJSFile: todoId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function markIncomplete() {
+  const todoId = this.parentNode.dataset.id;
+  try {
+    const response = await fetch("todos/markIncomplete", {
       method: "put",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
