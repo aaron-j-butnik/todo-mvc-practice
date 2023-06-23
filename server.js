47b099fd1a8a5ed0d2950bed4c78ebf0d2961961
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-22 18:11:10 -0700

    Created views directory with index.js file and associated ejs.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const homeRoutes = require("./routes/home");
 
 require("dotenv").config({ path: "./config/.env" });
+
+app.set("view engine", "ejs");
+
+app.use("/", homeRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
