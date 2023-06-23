14d9465883e42b9a0c8420d951db3eeabb31f841
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-22 18:38:58 -0700

    Added async function to connect to mongoDB in database.js and exported as connectDB variable in database.js. Requrired connectDB exported variable, and called connectDB() in server.js.  Succefully tested database connection.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/database");
 const homeRoutes = require("./routes/home");
 
 require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
 
 app.set("view engine", "ejs");
 
